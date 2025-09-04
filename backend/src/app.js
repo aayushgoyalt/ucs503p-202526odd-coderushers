dc635d30d58f55a0d2b3a281fbb742dea68fda8a
Anik2109 <anik.kaushesh@gmail.com>
2025-09-04 16:14:28 +0530

    Db connected

---
+++ backend/src/app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
+
+
+const allowedOrigins = [
+  "http://localhost:5173"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("❌ Not allowed by CORS"));
+      }
+    },
+    credentials: true
+  })
+);
+
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+app.get("/api/ping", (req, res) => {
+  console.log("✅ /ping route hit");
+  res.send("pong");
+});
+
+//routes import
+
+
+//routes decalaration
+
+
+
+export { app }
