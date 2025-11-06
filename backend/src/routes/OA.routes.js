8be4b12c768ab10123337cb9797075463ffc09fa
Anik2109 <anik.kaushesh@gmail.com>
2025-11-06 16:28:01 +0530

    feat: Add endOA functionality to terminate active OAs and update routes

--- backend/src/routes/OA.routes.js
+++ backend/src/routes/OA.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { verifyExtension,createOA ,getOAstatus,deleteOA,validateSubmission,getOAhistory} from "../controller/OA.controller.js";
+import { verifyExtension,createOA ,getOAstatus,deleteOA,validateSubmission,getOAhistory,endOA} from "../controller/OA.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {verifyAdmin} from "../middlewares/admin.middleware.js";
 
@@ -10,6 +10,7 @@ router.route("/create").post(verifyJWT, createOA);
 router.route("/status").get(verifyJWT, getOAstatus);
 router.route("/submit").post(validateSubmission);
 router.route("/delete").delete(verifyJWT, verifyAdmin, deleteOA);
+router.route("/end").post(verifyJWT, endOA);
 router.route("/history").get(verifyJWT, getOAhistory);
 
 export default router;
